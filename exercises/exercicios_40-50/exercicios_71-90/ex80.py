b1c071d598677670b10beba137a359e90304dfbd
mariacarneir0 <claramaria03062010@gmail.com>
2024-07-13 09:09:05 -0300

    feito o comite

--- exercises/exercicios_40-50/exercicios_71-90/ex80.py
+++ exercises/exercicios_40-50/exercicios_71-90/ex80.py
@@ -1,19 +1,21 @@
 
 dicionario1 = {
-    'chave1': 'valor1',
-    'chave2': 'valor2',
-    'chave3': 'valor3'
+   
+ "Brasil": "Brasília",
+    "França": "Paris",
+    
 }
+
 
 
 dicionario2 = {
-    'chave4': 'valor4',
-    'chave5': 'valor5',
-    'chave6': 'valor6'
+   "Japão": "Tóquio",
+    "Canadá": "Ottawa",
+    "Austrália": "Camberra"
 }
 
 
-dicionario_combinado = {**dicionario1, **dicionario2}
+combinando_dicionario = {'dicionario1', 'dicionario2'}
 
-print("Dicionário combinado:")
-print(dicionario_combinado)
+print("combinando_dicionario:")
+print(combinando_dicionario)
